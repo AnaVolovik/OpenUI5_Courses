bd7138befc8403667678c2779df94960663c6cc8
AnaVolovik <nastasia.volovik@gmail.com>
2024-10-26 17:40:43 +0300

    feat: Clear models before loading new data in header, material, and group functions

--- sap.f.tutorial.fiori2.13/webapp/controller/Detail.controller.js
+++ sap.f.tutorial.fiori2.13/webapp/controller/Detail.controller.js
@@ -35,7 +35,6 @@ sap.ui.define([
 			this._item = oEvent.getParameter("arguments").item || "0";
 
 			const itemPath = "/" + this._item;
-
 			this.getView().bindElement({
 				path: itemPath,
 				model: "items",
@@ -47,86 +46,95 @@ sap.ui.define([
 
 		_onBindingChange: function () {
 			const oModel = this.getView().getModel("items"),
-						context = this.getView().getBindingContext("items");
+				context = this.getView().getBindingContext("items");
 	
 			if (context) {
-					const headerID = context.getProperty("HeaderID");
-					const materialID = context.getProperty("MaterialID");
-					const groupID = context.getProperty("GroupID");
-	
-					this._loadHeaderData(headerID);
-					this._loadMaterialData(materialID);
-					this._loadGroupData(groupID);
+				const headerID = context.getProperty("HeaderID");
+				const materialID = context.getProperty("MaterialID");
+				const groupID = context.getProperty("GroupID");
+
+				this._loadHeaderData(headerID);
+				this._loadMaterialData(materialID);
+				this._loadGroupData(groupID);
 			}
 		},
 
 		_loadHeaderData: async function (headerID) {
 			const oModel = this.getView().getModel();
+
+			this.getView().setModel(new sap.ui.model.json.JSONModel({}), "headerData");
+    	this.getView().setBindingContext(null, "headerData");
 	
 			try {
-					const oData = await new Promise((resolve, reject) => {
-							oModel.read(`/zjblessons_base_Headers('${headerID}')`, {
-									success: function (data) {
-											resolve(data);
-									},
-									error: function (oError) {
-											reject(oError);
-									}
-							});
+				const oData = await new Promise((resolve, reject) => {
+					oModel.read(`/zjblessons_base_Headers('${headerID}')`, {
+						success: function (data) {
+							resolve(data);
+						},
+						error: function (oError) {
+							reject(oError);
+						}
 					});
-	
-					const headerModel = new sap.ui.model.json.JSONModel(oData);
-					this.getView().setModel(headerModel, "headerData");
-					this.getView().setBindingContext(headerModel.createBindingContext("/"), "headerData");
+				});
+
+				const headerModel = new sap.ui.model.json.JSONModel(oData);
+				this.getView().setModel(headerModel, "headerData");
+				this.getView().setBindingContext(headerModel.createBindingContext("/"), "headerData");
 			} catch (error) {
-					console.error("Error fetching Header data:", error);
+				console.error("Error fetching Header data:", error);
 			}
 		},
 	
 		_loadMaterialData: async function (materialID) {
 			const oModel = this.getView().getModel();
+
+			this.getView().setModel(new sap.ui.model.json.JSONModel({}), "materialData");
+    	this.getView().setBindingContext(null, "materialData");
 	
 			try {
-					const oData = await new Promise((resolve, reject) => {
-							oModel.read(`/zjblessons_base_Materials('${materialID}')`, {
-									success: function (data) {
-											resolve(data);
-									},
-									error: function (oError) {
-											reject(oError);
-									}
-							});
+				const oData = await new Promise((resolve, reject) => {
+					oModel.read(`/zjblessons_base_Materials('${materialID}')`, {
+						success: function (data) {
+							resolve(data);
+						},
+						error: function (oError) {
+							reject(oError);
+						}
 					});
-	
-					const materialModel = new sap.ui.model.json.JSONModel(oData);
-					this.getView().setModel(materialModel, "materialData");
-					this.getView().setBindingContext(materialModel.createBindingContext("/"), "materialData");
+				});
+
+				const materialModel = new sap.ui.model.json.JSONModel(oData);
+				this.getView().setModel(materialModel, "materialData");
+				this.getView().setBindingContext(materialModel.createBindingContext("/"), "materialData");
 			} catch (error) {
-					console.error("Error fetching Material data:", error);
+				console.error("Error fetching Material data:", error);
 			}
 		},
 	
 		_loadGroupData: async function (groupID) {
 			const oModel = this.getView().getModel();
+
+			this.getView().setModel(new sap.ui.model.json.JSONModel({}), "groupData");
+    	this.getView().setBindingContext(null, "groupData");
 	
 			try {
-					const oData = await new Promise((resolve, reject) => {
-							oModel.read(`/zjblessons_base_Groups('${groupID}')`, {
-									success: function (data) {
-											resolve(data);
-									},
-									error: function (oError) {
-											reject(oError);
-									}
-							});
+				const oData = await new Promise((resolve, reject) => {
+					oModel.read(`/zjblessons_base_Groups('${groupID}')`, {
+						success: function (data) {
+							resolve(data);
+						},
+						error: function (oError) {
+							reject(oError);
+						}
 					});
-	
-					const groupModel = new sap.ui.model.json.JSONModel(oData);
-					this.getView().setModel(groupModel, "groupData");
-					this.getView().setBindingContext(groupModel.createBindingContext("/"), "groupData");
-					console.log("Group Data:", oData);
+				});
+
+				const groupModel = new sap.ui.model.json.JSONModel(oData);
+				this.getView().setModel(groupModel, "groupData");
+				this.getView().setBindingContext(groupModel.createBindingContext("/"), "groupData");
+				console.log("Group Data:", oData);
 			} catch (error) {
-					console.error("Error fetching Group data:", error);
+				console.error("Error fetching Group data:", error);
 			}
 		},
 
