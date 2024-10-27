4ede24bcfd11df7db5b7d8cfb04d3abf8a672a8a
AnaVolovik <nastasia.volovik@gmail.com>
2024-10-27 17:11:20 +0300

    feat: add validation for name and surname fields

--- AppForm/webapp/controller/View1.controller.js
+++ AppForm/webapp/controller/View1.controller.js
@@ -4,6 +4,19 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("AppForm.controller.View1", {
+		_onNameInputLiveChange: function(oEvent) {
+			const nameRegex = /^[А-Яа-яЁё\s-]+$/;
+			const sValue = oEvent.getParameter("value");
+
+			if (nameRegex.test(sValue)) {
+					oEvent.getSource().setValueState("None");
+					oEvent.getSource().setValueStateText(""); 
+			} else {
+					oEvent.getSource().setValueState("Error");
+					oEvent.getSource().setValueStateText("Поле может содержать только кириллицу, пробелы и дефисы");
+			}
+		},
+
 		_onPhoneInputLiveChange: function(oEvent) {
 			const phoneRegex = /^\+375\s?\(?\d{2}\)?\s?\d{3}[-\s]?\d{2}[-\s]?\d{2}$/,
 						sValue = oEvent.getParameter("value");
@@ -16,5 +29,6 @@ sap.ui.define([
 					oEvent.getSource().setValueStateText("Введите номер в международном формате, начиная с +375");
 			}
 		}
+
 	});
 });
