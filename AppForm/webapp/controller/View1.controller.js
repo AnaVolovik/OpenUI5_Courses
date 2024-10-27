636a0c4e86e71214b118ae4ef3a753432382beb9
AnaVolovik <nastasia.volovik@gmail.com>
2024-10-27 17:04:50 +0300

    feat: add validation for Belarusian phone number format

--- AppForm/webapp/controller/View1.controller.js
+++ AppForm/webapp/controller/View1.controller.js
@@ -4,6 +4,17 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("AppForm.controller.View1", {
+		_onPhoneInputLiveChange: function(oEvent) {
+			const phoneRegex = /^\+375\s?\(?\d{2}\)?\s?\d{3}[-\s]?\d{2}[-\s]?\d{2}$/,
+						sValue = oEvent.getParameter("value");
 
+			if (phoneRegex.test(sValue)) {
+					oEvent.getSource().setValueState("None");
+					oEvent.getSource().setValueStateText("");
+			} else {
+					oEvent.getSource().setValueState("Error");
+					oEvent.getSource().setValueStateText("Введите номер в международном формате, начиная с +375");
+			}
+		}
 	});
 });
