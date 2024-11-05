f92672c0daef119866d9df3cad4d89bea7e145e8
AnaVolovik <nastasia.volovik@gmail.com>
2024-11-05 21:20:14 +0300

    chore: remove unnecessary indentation

--- AppForm/webapp/Component.js
+++ AppForm/webapp/Component.js
@@ -1,38 +1,38 @@
 sap.ui.define([
-	"sap/ui/core/UIComponent",
-	"sap/ui/Device",
-	"AppForm/model/models",
-	"sap/ui/model/json/JSONModel",
+  "sap/ui/core/UIComponent",
+  "sap/ui/Device",
+  "AppForm/model/models",
+  "sap/ui/model/json/JSONModel",
 ], function(UIComponent, Device, models, JSONModel) {
-	"use strict";
+  "use strict";
 
-	return UIComponent.extend("AppForm.Component", {
+  return UIComponent.extend("AppForm.Component", {
 
-		metadata: {
-			manifest: "json"
-		},
+    metadata: {
+      manifest: "json"
+    },
 
-		init: function() {
-			UIComponent.prototype.init.apply(this, arguments);
+    init: function() {
+      UIComponent.prototype.init.apply(this, arguments);
 
-			const oCityModel = new JSONModel({
-				cities: [
-						{ id: "Minsk", name: "Минск" },
-						{ id: "Grodno", name: "Гродно" },
-						{ id: "Brest", name: "Брест" },
-						{ id: "Gomel", name: "Гомель" },
-						{ id: "Mogilev", name: "Могилев" },
-						{ id: "Vitebsk", name: "Витебск" }
-				]
-			});
-			this.setModel(oCityModel, "cityModel");
+      const oCityModel = new JSONModel({
+        cities: [
+          { id: "Minsk", name: "Минск" },
+          { id: "Grodno", name: "Гродно" },
+          { id: "Brest", name: "Брест" },
+          { id: "Gomel", name: "Гомель" },
+          { id: "Mogilev", name: "Могилев" },
+          { id: "Vitebsk", name: "Витебск" }
+        ]
+      });
+      this.setModel(oCityModel, "cityModel");
 
-			const oAppModel = new sap.ui.model.json.JSONModel({
+      const oAppModel = new sap.ui.model.json.JSONModel({
         busy: false
-			});
-			this.setModel(oAppModel, "appModel");
+      });
+      this.setModel(oAppModel, "appModel");
 
-			this.setModel(models.createDeviceModel(), "device");
-		}
-	});
+      this.setModel(models.createDeviceModel(), "device");
+    }
+  });
 });
