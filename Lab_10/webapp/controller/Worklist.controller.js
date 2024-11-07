a1f82cc75a5d007ddedd7bf03534b07c3525215a
AnaVolovik <nastasia.volovik@gmail.com>
2024-11-07 16:50:51 +0300

    feat: set countMode to Inline and output the count value in header

--- Lab_10/webapp/controller/Worklist.controller.js
+++ Lab_10/webapp/controller/Worklist.controller.js
@@ -1,22 +1,43 @@
 sap.ui.define([
-    "Lab_10/Lab_10/controller/BaseController",
-    "sap/ui/model/json/JSONModel",
-    "Lab_10/Lab_10/model/formatter",
-    "sap/ui/model/Filter",
-    "sap/ui/model/FilterOperator"
-  ], function (BaseController, JSONModel, formatter, Filter, FilterOperator) {
-    "use strict";
+  "Lab_10/Lab_10/controller/BaseController",
+  "sap/ui/model/json/JSONModel",
+  "Lab_10/Lab_10/model/formatter",
+  "sap/ui/model/Filter",
+  "sap/ui/model/FilterOperator"
+], function (BaseController, JSONModel, formatter, Filter, FilterOperator) {
+  "use strict";
 
-    return BaseController.extend("Lab_10.Lab_10.controller.Worklist", {
+  return BaseController.extend("Lab_10.Lab_10.controller.Worklist", {
 
-      formatter: formatter,
+    formatter: formatter,
 
-      onInit : function () {
-        const oViewModel = new JSONModel({
+    onInit : function () {
 
+      const oViewModel = new JSONModel({
+        sCount: '0'
+      });
+      this.setModel(oViewModel, "worklistView");
+    },
+
+    onBeforeRendering: function () {
+      this._getTableCounter();
+    },
+
+    _getTableCounter() {
+      const oModel = this.getOwnerComponent().getModel(),
+            oTable = this.byId("table"),
+            oBinding = oTable.getBinding("items");
+
+      if (oBinding) {
+        oBinding.attachEvent("dataReceived", () => {
+          oModel.mCodeListModelParams.defaultCountMode = sap.ui.model.odata.CountMode.Inline;
+          const iCount = oBinding.getLength();
+          this.getView().getModel("worklistView").setProperty("/sCount", iCount);
         });
-        this.setModel(oViewModel, "worklistView");
-      },
-    });
-  }
+      }
+    },
+
+
+  });
+}
 );
