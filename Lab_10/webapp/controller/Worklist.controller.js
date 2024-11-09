6ad87711d82b93c3baee81a1e606a2ee3cfcc10e
AnaVolovik <nastasia.volovik@gmail.com>
2024-11-09 12:51:11 +0300

    feat: switch from OData model to JSON model and refactor functions

--- Lab_10/webapp/controller/Worklist.controller.js
+++ Lab_10/webapp/controller/Worklist.controller.js
@@ -10,57 +10,86 @@ sap.ui.define([
     formatter: formatter,
 
     onInit : function () {
+      const oModel = this.getOwnerComponent().getModel(),
+            sPath = "/zjblessons_base_Headers";
 
       const oViewModel = new JSONModel({
-        sCount: '0',
-        isBusy: false
+        sCount: "0",
+        isBusy: true
       });
       this.setModel(oViewModel, "worklistView");
 
-      const oModel = this.getOwnerComponent().getModel();
-      oModel.setDefaultBindingMode(sap.ui.model.BindingMode.OneWay);
+      const oDataPromise = new Promise((resolve, reject) => {
+        oModel.read(sPath, {
+          success: (oData) => {
+            this.oJsonModel = new sap.ui.model.json.JSONModel(oData.results);
+            this.oJsonModel.setDefaultBindingMode(sap.ui.model.BindingMode.OneWay);
+
+            this.getView().setModel(this.oJsonModel, "myJsonModel");
+
+            resolve(this.oJsonModel);
+          },
+          error: (oError) => {
+            console.error("Error fetching data", oError);
+            oViewModel.setProperty("/isBusy", false);
+            reject(oError);
+          }
+        });
+      });
+
+      oDataPromise
+        .then((oJsonModel) => {
+          const oTable = this.byId("table"),
+                oBinding = oTable.getBinding("items");
+
+          const oDefaultSorter = new sap.ui.model.Sorter("DocumentNumber", false);
+          oBinding.sort([oDefaultSorter]);
+
+          this._getTableCounter(oJsonModel);
+
+          oViewModel.setProperty("/isBusy", false);
+        })
+        .catch((oError) => {
+          oViewModel.setProperty("/isBusy", false);
+          console.error("Error during data fetch:", oError);
+        });
     },
 
-    onBeforeRendering: function () {
-      this._getTableCounter();
-    },
-
-    _getTableCounter() {
-      const oModel = this.getOwnerComponent().getModel(),
+    _getTableCounter(oJsonModel) {
+      const oModelData = oJsonModel.getData(),
             oTable = this.byId("table"),
             oBinding = oTable.getBinding("items");
 
       if (oBinding) {
-        oBinding.attachEvent("dataReceived", () => {
-          oModel.mCodeListModelParams.defaultCountMode = sap.ui.model.odata.CountMode.Inline;
-          const iCount = oBinding.getLength();
-          this.getView().getModel("worklistView").setProperty("/sCount", iCount);
-        });
+        oJsonModel.setDefaultBindingMode(sap.ui.model.BindingMode.TwoWay);
+        const iCount = oModelData.length;
+        this.getView().getModel("worklistView").setProperty("/sCount", iCount);
       }
     },
 
-
     onChangeDescription (oEvent) {
-      const oBindingContext = oEvent.getSource().getBindingContext(),
+      const oInput = oEvent.getSource(),
+            oBindingContext = oInput.getBindingContext("myJsonModel"),
+            sPath = oBindingContext.getPath(),
             sNewValue = oEvent.getParameter("newValue");
 
-      oBindingContext.getModel().setProperty("Description", sNewValue, oBindingContext);
+      oBindingContext.getModel().setProperty(sPath + "/Description", sNewValue);
       oBindingContext.getModel().refresh(true);
     },
 
     onGroupRegionText () {
       const oTable = this.byId("table"),
             oBinding = oTable.getBinding("items"),
-            bGrouped = oTable.getBinding("items").isGrouped();
+            bGrouped = oBinding.isGrouped();
 
       const oViewModel = this.getView().getModel("worklistView");
       oViewModel.setProperty("/isBusy", true);
 
       if (bGrouped) {
-        const oDefaultSorter = new sap.ui.model.Sorter("DocumentNumber", true);
+        const oDefaultSorter = new sap.ui.model.Sorter("DocumentNumber", false);
         oBinding.sort([oDefaultSorter]);
       } else {
-        const oSorter = new sap.ui.model.Sorter("RegionText", true, true);
+        const oSorter = new sap.ui.model.Sorter("RegionText", false, true);
         oBinding.sort([oSorter]);
       }
 
