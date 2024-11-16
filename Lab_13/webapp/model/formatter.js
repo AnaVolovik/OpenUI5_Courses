c743bed98135a848bcebac1e18fdc8c3c3cc0c6f
AnaVolovik <nastasia.volovik@gmail.com>
2024-11-16 10:57:29 +0300

    fix: refine formatter

--- Lab_13/webapp/model/formatter.js
+++ Lab_13/webapp/model/formatter.js
@@ -4,14 +4,21 @@ sap.ui.define([], function () {
   return {
     checkIcon: function (createdByAvatar, createdByFullName) {
       if (createdByFullName && createdByFullName.trim() !== "") {
+        if (createdByAvatar && createdByAvatar.trim() !== "") {
+          return createdByAvatar;
+        }
         return "sap-icon://customer";
       }
 
-      if (createdByAvatar && createdByAvatar.trim() !== "") {
-        return createdByAvatar;
-      }
+      return "sap-icon://customer";
+    },
 
-      return "";
+    checkIconColor: function (createdByFullName) {
+      return createdByFullName && createdByFullName.trim() !== "" ? "Positive" : "Negative";
+    },
+
+    checkName: function (createdByFullName) {
+      return createdByFullName && createdByFullName.trim() !== "" ? createdByFullName : "Unknown";
     }
   };
 });
